5d05ad32c1e5fde0104d87f4c8be5e5bab585171
Do Thanh <thanhcoral@gmail.com>
2022-09-08 17:28:26 +0700

    up

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 
 from .forms import LoginForm, RegisterForm
 
 
 def login_view(request):
     form = LoginForm(request.POST or None)
-    if request.user is not None:
-        return redirect('/')
+    # if request.user is not None:
+    #     return redirect('/')
     if request.method == 'POST':
         if form.is_valid():
             username = form.cleaned_data.get("username")
@@ -31,6 +31,7 @@ def login_view(request):
 def logout_view(request):
     pass
 
+@permission_required("user.add_user", raise_exception=True)
 def register_view(request):
     form = RegisterForm(request.POST or None)
     if request.method == 'POST':
--- common/user_level.py
+++ common/user_level.py
@@ -1,12 +1,12 @@
 SYSTEM_ADMIN = 0
 SUB_ADMIN = 1
-ACCOUNTANT = 2
-AR_OFFICER = 3
-AP_OFFICER = 4
-PRODUCTION_MANAGER = 5
-PURCHASE_MANAGER = 6
-STOCK_MANAGER = 7
-SALE_MAN = 8
+# ACCOUNTANT = 2
+# AR_OFFICER = 3
+# AP_OFFICER = 4
+# PRODUCTION_MANAGER = 5
+# PURCHASE_MANAGER = 6
+# STOCK_MANAGER = 7
+# SALE_MAN = 8
 
 INQUIRIES = 10
 
@@ -14,12 +14,12 @@ INQUIRIES = 10
 USER_LEVEL_CHOICES = (
     (SYSTEM_ADMIN, "System admin"),
     (SUB_ADMIN, "Sub admin"),
-    (ACCOUNTANT, "Accountant"),
-    (AR_OFFICER, "AR Officer"),
-    (AP_OFFICER, "AP Officer"),
-    (PRODUCTION_MANAGER, "Production manager"),
-    (PURCHASE_MANAGER, "Purchase manager"),
-    (STOCK_MANAGER, "Stock manager"),
-    (SALE_MAN, "Sale man"),
+    # (ACCOUNTANT, "Accountant"),
+    # (AR_OFFICER, "AR Officer"),
+    # (AP_OFFICER, "AP Officer"),
+    # (PRODUCTION_MANAGER, "Production manager"),
+    # (PURCHASE_MANAGER, "Purchase manager"),
+    # (STOCK_MANAGER, "Stock manager"),
+    # (SALE_MAN, "Sale man"),
     (INQUIRIES, "Inquiries"),
 )
