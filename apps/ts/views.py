5181d6b5b3e243be3ee0dd158bae89ec7cb8b5ed
Do Thanh <thanhcoral@gmail.com>
2022-09-09 12:41:06 +0700

    up

--- apps/ts/views.py
+++ apps/ts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from common.permissions import group_required
 
-@group_required('Inquiries')
+@group_required('sub_admin')
 def humanResources(request):
     context = {}
     return render(request, 'pages/human-resources.html', context)
