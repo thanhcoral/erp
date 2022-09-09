39b7617ff873d65a40a50e04a3b0670f616f3c6d
Do Thanh <thanhcoral@gmail.com>
2022-09-09 12:53:30 +0700

    up

--- apps/ts/views.py
+++ apps/ts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from common.permissions import group_required
 
-@group_required('sub_admin')
+@group_required('Sub Admin', raise_exception=True)
 def humanResources(request):
     context = {}
     return render(request, 'pages/human-resources.html', context)
--- common/permissions.py
+++ common/permissions.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.core.exceptions import PermissionDenied
 
-def group_required(*group_names):
+
+def group_required(*group_names, login_url=None, raise_exception=False):
     """Requires user membership in at least one of the groups passed in."""
     def in_groups(u):
         if u.is_authenticated:
             if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
                 return True
+        if raise_exception:
+            raise PermissionDenied
         return False
 
-    return user_passes_test(in_groups, login_url='403')
+    return user_passes_test(in_groups, login_url=login_url)
