486840afc934bd1c600682036e2025beb9352a8f
Do Thanh <thanhcoral@gmail.com>
2022-09-09 10:23:42 +0700

    up

--- apps/ts/views.py
+++ apps/ts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
 
 
+@permission_required("user.view_user", raise_exception=True)
 def humanResources(request):
     context = {}
     return render(request, 'pages/human-resources.html', context)
